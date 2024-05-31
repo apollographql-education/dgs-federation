70b77b0753c56e2aa812ff4f597ecefecd1bcea8
Liz Hennessy <95302380+lizhennessy@users.noreply.github.com>
2024-05-30 20:51:19 -0600

    Update ListingModel.java

--- listings/src/main/java/com/example/listings/models/ListingModel.java
+++ listings/src/main/java/com/example/listings/models/ListingModel.java
@@ -1,11 +1,8 @@
 package com.example.listings.models;
 
 
-import com.example.listings.generated.types.Amenity;
 import com.example.listings.generated.types.Listing;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.List;
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
